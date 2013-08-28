90ae37fd6401dd5ad7d98a07a1bd4e7ef014380d
Marc Reynolds <reynolds.marc@gmail.com>
2013-08-28 15:50:54 +0800

    routes changed to append

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
-Spree::Core::Engine.routes.draw do
+Spree::Core::Engine.routes.append do
   match "/admin/orders/:id/print", :to => 'admin/orders#print', :via => :get, :as => :admin_print_order
 end
